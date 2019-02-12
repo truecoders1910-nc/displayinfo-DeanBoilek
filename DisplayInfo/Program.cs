cd3feb6a3a3349e6152f30a52945455d6a1b35e4
bdowds <bwdowds@gmail.com>
2019-02-12 16:20:46 -0600

    Added comments for project

--- DisplayInfo/Program.cs
+++ DisplayInfo/Program.cs
@@ -6,7 +6,15 @@ namespace DisplayInfo
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // Get first and last name from user input
+            // Get their favorite number
+            // Get their favorite animal
+
+            // Once you have gotten all their info and stored it,
+            // display it back to them.
+
+            // If their favorite number is greater than 10, display it in green.
+            // If their favorite number is less than 5, display it in red.
         }
     }
 }
